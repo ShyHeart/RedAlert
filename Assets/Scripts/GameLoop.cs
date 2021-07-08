a1532d0d6a37e2cebd2643811ab39a1646240dbe
ShyHeart <18296998265@163.com>
2021-07-08 16:38:58 +0800

    创建GameLoop 使用脚本与游戏物体分离的开发模式

---
+++ Assets/Scripts/GameLoop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//脚本与游戏物体分离的开发模式
+public class GameLoop : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
